dcc1cce57157ba5ee612ad1cb6f6583ea65f4a3c
cg07978 <54293611+cg07978@users.noreply.github.com>
2022-02-27 11:18:16 -0500

    Commented src\components\Home.vue
    Commented src\components\Home.vue

--- program-repair-tool/src/components/Home.vue
+++ program-repair-tool/src/components/Home.vue
@@ -1,3 +1,9 @@
+<!--
+This is the templete for the home component.
+In it is a div with the home-container styling class, and in that div is a header that says "Home".
+The tag <problems/> tells Vue to insert the component named "problems" in this location
+-->
+
 <template>
     <div class="home-container">
         <h1>Home</h1>
@@ -7,6 +13,13 @@
 </template>
 
 <script>
+
+/*
+The problems component is imported from the Problems.vue file
+It is then registered as a component, meaning it is visible to the template, by exporting it within the components field.
+This is described here: https://vuejs.org/guide/essentials/component-basics.html#using-a-component
+*/
+
 import Problems from "./Problems.vue"
 export default {
     components: {
@@ -14,6 +27,12 @@ export default {
     },
 }
 </script>
+
+<!--
+This is the component's style.
+In it is the CSS the component will use, which in this case is just the class called home-container, which has some specific settings associated with it.
+-->
+
 <style>
     home-container {
         margin: auto;
