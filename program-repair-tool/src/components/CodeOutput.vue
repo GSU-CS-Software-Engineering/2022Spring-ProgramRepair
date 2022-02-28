3d3593543f292d6a4560524857d7ed81a5628db0
cg07978 <54293611+cg07978@users.noreply.github.com>
2022-02-28 14:26:22 -0500

    Added comments to src\components\CodeOutput.vue
    Added comments to src\components\CodeOutput.vue

--- program-repair-tool/src/components/CodeOutput.vue
+++ program-repair-tool/src/components/CodeOutput.vue
@@ -1,7 +1,19 @@
+<!--
+Right now there is a minor uncertainty on line 31.
+-->
+<!--
+Below is this component's template.
+First is a div that has an attribute called output, which is set to always equal the value of the output prop below.
+-->
 <template>
     <div class="code-output" v-bind:output="output" >
         <h3 class="header">Stdout:</h3>
         <ul class="output">
+            <!--
+            The code below creates a list element for every entry inside of the output arrary.
+            The :key attribute is used to accurately iterate through the area.
+            I do not understand the deep semantics of this, but it seems to just be a way to identify each entry and works appropriately.
+            -->
             <li
                 v-for="entry in output" 
                 :key="entry">
@@ -13,6 +25,12 @@
 
 <script>
 export default {
+
+    /*
+    Below this component is given a name and a prop, which is an array called output.
+    I am uncertain why this component has a name while others do not, or even if it matters.
+    */
+
     name: 'CodeOutput',
     props: {
         output: Array
@@ -20,6 +38,11 @@ export default {
     
 }
 </script>
+
+<!--
+Below is the style, and again, the scoped boolean is deprecated.
+Not sure why the removed the margin setting, may experiment with that.
+-->
 
 <style scoped>
     ul {
